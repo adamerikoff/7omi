899fe42577f34df843653f81632b73aee4f582f4
adamerikoff <adamerikoff@protonmail.com>
2024-02-26 20:19:18 +0600

    started chat dashboard

--- 7omi_client_web/src/App.tsx
+++ 7omi_client_web/src/App.tsx
@@ -6,8 +6,12 @@ import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 
 import Home from './components/home/Home'
+
+import Chat from './components/chat/Chat'
+
 import Login from './components/login/Login'
 import Signup from './components/signup/Signup'
+
 import About from './components/statics/About'
 import Contacts from './components/statics/Contacts'
 import NotFound from './components/statics/NotFound'
@@ -23,6 +27,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/about" element={<About />} />
         <Route path="/contacts" element={<Contacts />} />
+        <Route path="/chat" element={<Chat />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer/>
