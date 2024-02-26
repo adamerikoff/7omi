eea187c10c64f6e99093274c6af868c4d832c1d8
adamerikoff <adamerikoff@protonmail.com>
2024-02-26 12:13:30 +0600

    .

--- 7omi_client_web/src/components/navbar/Navbar.tsx
+++ 7omi_client_web/src/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import './Navbar.css'
 function Navbar() {
   return (
     <nav className='w-screen bg-sky-500'>
-      <div className='flex justify-center'>
+      <div className='flex justify-center py-3'>
         <Link to="/" className="mx-4 text-white">Home</Link>
         <Link to="/about" className="mx-4 text-white">About</Link>
         <Link to="/contacts" className="mx-4 text-white">Contacts</Link>
