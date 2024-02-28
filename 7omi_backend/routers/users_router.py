cd359f6a9d7d9f4248067007f4dc252fa734b6cc
adamerikoff <adamerikoff@protonmail.com>
2024-02-28 20:34:32 +0600

    users api 80% finished

--- 7omi_backend/routers/users_router.py
+++ 7omi_backend/routers/users_router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Response, Depends
 
 from sqlalchemy.orm import Session
 
 from models import models
+
+from pydantic_schemas import user_schemas
 
 from db import get_db
 
@@ -24,8 +26,16 @@ async def read_user_me(db: Session = Depends(get_db)):
   return {"username": "fakecurrentuser"}
 
 @router.post("/")
-async def create_new_user(db: Session = Depends(get_db)):
-  return f"created user"
+async def create_new_user(user_data: user_schemas.CreateUser, db: Session = Depends(get_db)):
+  existing_user = db.query(models.User).filter(models.User.username == user_data.username).first()
+  if existing_user:
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username is already taken")
+  hashed_password = models.User.hash_password(user_data.password)
+  new_user = models.User(username=user_data.username, password=hashed_password)
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return new_user
 
 @router.get("/{user_id}")
 async def read_user(user_id: str, db: Session = Depends(get_db)):
@@ -38,6 +48,11 @@ async def read_user(user_id: str, db: Session = Depends(get_db)):
 async def update_chat(user_id: int, db: Session = Depends(get_db)):
   return f"updated user{user_id}"
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_chat(user_id: int, db: Session = Depends(get_db)):
-  return f"deleted user {user_id}"
+  user_query = db.query(models.User).filter_by(user_id=user_id)
+  if user_query.first() == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+  user_query.delete(synchronize_session=False)
+  db.commit()
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
