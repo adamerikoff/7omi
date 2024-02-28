01181d0e6e7bc56ac9f1c87f261382771cfda786
adamerikoff <adamerikoff@protonmail.com>
2024-02-28 17:31:20 +0600

    added sqlalchemy

--- 7omi_backend/main.py
+++ 7omi_backend/main.py
@@ -1,26 +1,40 @@
+
 from fastapi import FastAPI
+
+from sqlalchemy.orm import sessionmaker
+
 
 from routers import users_router, chats_router, messages_router  
 
+from models import user_model
 app = FastAPI()
 
 
-from sqlalchemy import create_engine
-
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+import sqlalchemy as db
 
 db_config = {
+  "dialect": "postgresql",
+  "engine": "psycopg2",
   "user": "postgres",
   "password": "postgres",
   "host": "localhost",
   "port": "5432",
-  "database": "7om"
+  "database": "7omi"
 }
+db_string = f"{db_config['dialect']}+{db_config['engine']}://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+
+engine = db.create_engine(db_string, echo=True)
+
+conn = engine.connect()
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-
-
-
+users = session.query(user_model.User).all()
+# Print the results
+for user in users:
+  print(f"ID: {user.user_id}, Name: {user.username}, Email: {user.password}")
 
 
 app.include_router(users_router.router)
