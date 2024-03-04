c68697902ed27f7bebfb5aae90fab117b337662d
adamerikoff <adamerikoff@protonmail.com>
2024-03-05 02:38:07 +0500

    removed everything

--- back/models/user.py
+++
@@ -1,8 +0,0 @@
-from beanie import Document, Indexed
-
-class UserDocument(Document):
-  username: Indexed(str, unique=True)
-  hashed_password: str
-  
-  class Settings:
-    name = "users_collection"
--- back/schemas/response.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class ResponseSchema(BaseModel):
-  status: int
-  detail: str
-
-
-
-
