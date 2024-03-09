fd72e31e78409fb9dad2540eddcc2803519ca261
adamerikoff <adamerikoff@protonmail.com>
2024-03-10 02:58:04 +0500

    user routes refactored with authorization

--- core/oauth2/oauth2.py
+++ core/oauth2/oauth2.py
@@ -12,7 +12,7 @@ SECRET_KEY = "7b1696555cddffdf04b50d17d5b7d81ea5212416a6bae20336869e77106b381d"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def create_acess_token(data: dict, expire_delta: Optional[timedelta] = None):
     to_encode = data.copy()
--- core/routers/users_router.py
+++ core/routers/users_router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status, HTTPException, Response
+from fastapi import APIRouter, status, HTTPException, Response, Depends
 
 from core.schemas.user import UserBase, UserCreate, UserDB, UserUpdate
 from core.documents.user import UserDocument 
 from core.documents.user import check_uniqueness, hash_password, verify_password, get_user_by_username, get_users
+from core.oauth2 import oauth2
 
 router = APIRouter(
     prefix="/users",
@@ -13,17 +14,15 @@ router = APIRouter(
 )
 
 @router.get("/")
-async def retrieve_users():
+async def retrieve_users(current_user: UserDB = Depends(oauth2.get_current_user)):
     users = await get_users()
     if not users:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Users not found.")
     return [UserBase(**u.model_dump()) for u in users]
 
-@router.get("/{username}")
-async def retrieve_user(username: str):
-    user = await get_user_by_username(username)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
+@router.get("/me")
+async def retrieve_user(current_user: UserDB = Depends(oauth2.get_current_user)):
+    user = await current_user
     return UserBase(**user.model_dump())
     
 
@@ -39,26 +38,23 @@ async def create_user(new_user: UserCreate):
     await UserDocument.insert(user_document_representation)
     return UserBase(**user_document_representation.model_dump())
 
-@router.put("/{username}")
-async def update_user(username: str, user_data: UserUpdate):
+@router.put("/me")
+async def update_user(user_data: UserUpdate, current_user: UserDB = Depends(oauth2.get_current_user)):
     if user_data.password:
         user_data.hashed_password = hash_password(user_data.password)
     user_representation = UserDB(**user_data.model_dump())
-
     # Retrieve the user to update
-    user_to_update = await get_user_by_username(username)
-
+    user_to_update = await current_user
     if user_to_update is None:
         raise HTTPException(status_code=404, detail="User not found.")
-    
     user_to_update.__dict__.update(**user_representation.model_dump(exclude_unset=True))
     await user_to_update.save()
     return UserBase(**user_to_update.model_dump())
 
-@router.delete("/{username}")
-async def delete_user(username: str):
+@router.delete("/me")
+async def delete_user(current_user: UserDB = Depends(oauth2.get_current_user)):
     #check rights
-    user = await get_user_by_username(username)
+    user = await current_user
     if user is None:
         raise HTTPException(status_code=404, detail="User not found.")
     await user.delete()
